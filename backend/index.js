5e9d0f13759c031f3b83c62c10adb421f9d9ebc8
Vishrut8898 <vishrut9869@gmail.com>
2025-05-07 16:46:13 +0530

    added path in index file of backend

--- backend/index.js
+++ backend/index.js
@@ -7,18 +7,20 @@ import userRoute from "./routes/user.route.js";
 import companyRoute from "./routes/company.route.js";
 import jobRoute from "./routes/job.route.js";
 import applicationRoute from "./routes/application.route.js";
+import path from "path";
 
 dotenv.config({});
 
 const app = express();
+const __dirname = path.resolve();
 
 // middleware
 app.use(express.json({ limit: "10mb" }));
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'http://localhost:5173',
-    credentials:true
+    origin: 'http://localhost:5173',
+    credentials: true
 }
 
 app.use(cors(corsOptions));
@@ -32,9 +34,15 @@ app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicationRoute);
 
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+}
 
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
     connectDB();
     console.log(`Server Running at Port: ${PORT}`);
 })
